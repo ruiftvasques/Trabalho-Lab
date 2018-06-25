82fcee15c6b66066f97364ff1b1a35b788be69c1
Anita Carvalho <9160081@esmad.ipp.pt>
2018-06-25 17:56:08 +0100

    3v

--- server.js
+++ server.js
@@ -11,32 +11,63 @@ app.set("view engine","hbs");
 var darksky="699202146ac273eefb16cbf1b0792f6d";
 var GoogleKey="AIzaSyBlwJ-REINoderL06w26S_bcozh_83KFLc";
 
-// adereço apartir do terminal
-//const argv=yargs.argv;   
-//const address;
-
-var temp;
-var appTemp;
-var dewPoint;
+var time;
+var summary;
+var icon;
+var precipProbability;
+var temperature;
+var apparentTemperature;
 var humidity;
-var precipType;
-var precipIntensity;
-var precipIntensityError;
 var windSpeed;
-var visibility;
-var windGust;
+var uvIndex;
+
+
+//hora 0
+var summary1H0;
+var icon1H0;
+var timeH0;
+var summaryH0;
+var iconH0;
+var precipProbabilityH0;
+var temperatureH0;
+var apparentTemperatureH0;
+var humidityH0;
+var windSpeedH0;
+var uvIndexH0;
+
+
+//dia atual
+var summary1D0;
+var icon1D0;
+var timeD0;
+var summaryD0;
+var iconD0;
+var precipProbabilityD0;
+var temperatureHighD0;
+var temperatureLowD0;
+var humidityD0;
+var windSpeedD0;
+var uvIndexD0;
+
+//endereço usado
+var formatted_address;
+
 
 //objetos
-       app.get('/weather',(req, res)=>{
+       app.get('/',(req, res)=>{
         var pedido = req.query.local;
 
         var encodedAdress= encodeURIComponent(pedido);
 
         request({url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAdress}&key=${GoogleKey}`,
         json: true},(error,response,body) => {
+                if(body.results[0].geometry.location.lat==undefined && body.results[0].geometry.location.lng==undefined){
+                    console.log("rro");
+                }else{
                 var lat=body.results[0].geometry.location.lat;
                 var lng=body.results[0].geometry.location.lng;
-                var formatted_address= body.results[0].formatted_address;
+              }
+                formatted_address= body.results[0].formatted_address;
                 console.log("latitude:"+lat);
                 console.log("longitude:"+lng);
                 console.log("A morada é " +formatted_address);
@@ -44,54 +75,99 @@ var windGust;
                 request({url:`https://api.darksky.net/forecast/${darksky}/${lat},${lng}?units=si`,json: true
               },(DSerror,DSresponse,DSbody) => {
                 console.log(body);
-                  temp=DSbody.currently.temperature;
-                  appTemp=DSbody.currently.apparentTemperature;
-                  dewPoint=DSbody.currently.dewPoint;
+                  time=DSbody.currently.time;
+                  summary=DSbody.currently.summary;
+                  icon=DSbody.currently.icon
+                  precipProbability=DSbody.currently.precipProbability;
+                  temperature=DSbody.currently.temperature;
+                  apparentTemperature=DSbody.currently.apparentTemperature;
                   humidity=DSbody.currently.humidity;
-                  precipType=DSbody.currently.precipType;
-                  precipIntensity=DSbody.currently.precipIntensity;
-                  precipIntensityError=DSbody.currently.precipIntensityError;
                   windSpeed=DSbody.currently.windSpeed;
-                  visibility=DSbody.currently.visibility;
-                  windGust=DSbody.currently.windGust;
+                  uvIndex=DSbody.currently.uvIndex;
+                  
         
-                 //precipMinIntensity =DSbody.minutely.data.precipIntensity;
-        
-                  console.log("Temperatura: "+temp);
-                  console.log("Tema: "+appTemp);
-                  console.log("Tema: "+dewPoint);
-                  console.log("Tema: "+humidity);
-                  console.log("Tema: "+ precipType);
+
+                //hora atual
+                  summary1H0=DSbody.hourly.summary;
+                  icon1H0=DSbody.hourly.icon;
+                  timeH0=DSbody.hourly.data[0].time;
+                  summaryH0=DSbody.hourly.data[0].summary;
+                  iconH0=DSbody.hourly.data[0].icon;
+                  precipProbabilityH0=DSbody.hourly.data[0].precipProbability;
+                  temperatureH0=DSbody.hourly.data[0].temperature;
+                  apparentTemperatureH0=DSbody.hourly.data[0].apparentTemperature;
+                  humidityH0=DSbody.hourly.data[0].humidity;
+                  windSpeedH0=DSbody.hourly.data[0].windSpeed;
+                  uvIndexH0=DSbody.hourly.data[0].uvIndex;
+
+
+
+                  //dia ataul
+                  summary1D0=DSbody.daily.summary;
+                  icon1D0=DSbody.daily.icon;
+                  timeD0=DSbody.daily.data[0].time;
+                  summaryD0=DSbody.daily.data[0].summary;
+                  IconD0=DSbody.daily.data[0].icon;
+                  precipProbabilityD0=DSbody.daily.data[0].precipProbability;
+                  temperatureHighD0=DSbody.daily.data[0].temperatureHigh;
+                  temperatureLowD0=DSbody.daily.data[0].temperatureLow;
+                  humidityD0=DSbody.daily.data[0].humidity;
+                  windSpeedD0=DSbody.daily.data[0].windSpeed;
+                  uvIndexD0=DSbody.daily.data[0].uvIndex;
+                  
         
                  
                  });
         
-               });
+               
 
 
         res.render('welcome.hbs',{
-            title:"Meteorologia",
-            text1:"A temperatura hoje é:"+temp,
-            text2:"A temperatura neste momento é:"+appTemp,
-            text3:"A humidade é:"+humidity,
-            text4:"A intensidade da chuva é:"+precipIntensity,
-            text5:"O erro na intensidade da chuva é:"+precipIntensityError,
-            text6:"A velocidade do vento é:"+windSpeed,
-            text7:"hoje vai estar:"+ precipType,
+            title:"World Weather",
+            title1:"Atual",
+            subtitle:"Address: "+formatted_address,
+            text1:"time: "+time,
+            text2:summary,
+            text3:"icon: "+ icon,
+            text4:"precipProbability: "+precipProbability,
+            text5: temperature,
+            text6:"apparentTemperature: "+apparentTemperature,
+            text7:"humidity: "+humidity,
+            text8:"windSpeed: "+windSpeed,
+            text9:"uvIndex:"+uvIndex,
+            title2:"hora atual",
+            text10:"summary: "+summary1H0,
+            text11:"icon: "+icon1H0,
+            text12:"time: "+timeH0,
+            text13:"summary: "+summaryH0,
+            text14:"precipProbability: "+ precipProbabilityH0,
+            text15:"temperature: "+ temperatureH0,
+            text16:"apparentTemperature :"+apparentTemperatureH0,
+            text17:"humidity: "+humidityH0,
+            text18:"windSpeed: "+windSpeedH0,
+            text19:"uvIndex: "+uvIndexH0,
+            title3:"dia atual",
+            text20:"summary: "+summary1D0,
+            text21:"icon: "+icon1D0,
+            text22:"time: "+timeD0,
+            text23:"summary: "+summaryD0,
+            text24:"precipProbability: "+ precipProbabilityD0,
+            text25:"temperature high: "+ temperatureHighD0,
+            text26:"temperature low: "+ temperatureLowD0,
+            text27:"humidity: "+humidityD0,
+            text28:"windSpeed: "+windSpeedD0,
+            text29:"uvIndex: "+uvIndexD0,
+            
+            
+            
+
+          });
             
         });
         
         });
+      
 
-        app.get('/',(request, response)=>{
-          response.render("welcome.hbs");
-          
-         });
-
-
-      /*app.get('/',(request, response)=>{
-       response.render("welcome.hbs",{texto:request.query.local})
-      });*/
 
         app.listen(3500);
 
